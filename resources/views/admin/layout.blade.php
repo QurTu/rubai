b5876a82d1c0c964c51e21869782331a95054464
QurTu <martis55@inbox.lt>
2020-08-25 16:57:30 +0300

    cosnole error fixed

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -53,7 +53,12 @@
   </head>
 
   <body style='background-color: #ddd;'>
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js" ></script>
+  <script
+  src="https://code.jquery.com/jquery-3.5.0.min.js"
+  integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ="
+  crossorigin="anonymous"></script>
+  <script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js">
+      </script>
 
     <!-- ########## START: LEFT PANEL ########## -->
     <div class="sl-logo"><a href=""><i class="icon ion-android-star-outline"></i> Rubai</a></div>
@@ -203,7 +208,16 @@
   
     <!-- ########## END: MAIN PANEL ########## -->
 
-   
+    <script src="{{ asset('backend/lib/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('backend/lib/popper.js/popper.js') }}"></script>
+    <script src="{{ asset('backend/lib/bootstrap/bootstrap.js') }}"></script>
+    <script src="{{ asset('backend/lib/jquery-ui/jquery-ui.js') }}"></script>
+    <script src="{{ asset('backend/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js') }}"></script>
+    <script src="{{ asset('backend/lib/highlightjs/highlight.pack.js') }}"></script>
+    <script src="{{ asset('backend/lib/datatables/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('backend/lib/datatables-responsive/dataTables.responsive.js') }}"></script>
+    <script src="{{ asset('backend/lib/select2/js/select2.min.js') }}"></script>
+
 
     
 
@@ -260,22 +274,23 @@
             });
     </script>
 
+   
 
-    <script src="{{asset('backend/lib/popper.js/popper.js')}}"></script>
-    <script src="{{asset('backend/lib/bootstrap/bootstrap.js')}}"></script>
-    <script src="{{asset('backend/lib/jquery-ui/jquery-ui.js')}}"></script>
-    <script src="{{asset('backend/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js')}}"></script>
-    <script src="{{asset('backend/lib/jquery.sparkline.bower/jquery.sparkline.min.js')}}"></script>
-    <script src="{{asset('backend/lib/d3/d3.js')}}"></script>
-    <script src="{{asset('backend/lib/rickshaw/rickshaw.min.js')}}"></script>
-    <script src="{{asset('backend/lib/chart.js/Chart.js')}}"></script>
-    <script src="{{asset('backend/lib/Flot/jquery.flot.js')}}"></script>
-    <script src="{{asset('backend/lib/Flot/jquery.flot.pie.js')}}"></script>
-    <script src="{{asset('backend/lib/Flot/jquery.flot.resize.js')}}"></script>
-    <script src="{{asset('backend/lib/flot-spline/jquery.flot.spline.js')}}"></script>
 
-    <script src="{{asset('backend/js/starlight.js')}}"></script>
-    <script src="{{asset('backend/js/ResizeSensor.js')}}"></script>
-    <script src="{{asset('backend/js/dashboard.js')}}"></script>
+    <script src="{{ asset('backend/lib/jquery.sparkline.bower/jquery.sparkline.min.js') }}"></script>
+    <script src="{{ asset('backend/lib/d3/d3.js') }}"></script>
+    <script src="{{ asset('backend/lib/rickshaw/rickshaw.min.js') }}"></script>
+    <script src="{{ asset('backend/lib/chart.js/Chart.js') }}"></script>
+    <script src="{{ asset('backend/lib/Flot/jquery.flot.js') }}"></script>
+    <script src="{{ asset('backend/lib/Flot/jquery.flot.pie.js') }}"></script>
+    <script src="{{ asset('backend/lib/Flot/jquery.flot.resize.js') }}"></script>
+    <script src="{{ asset('backend/lib/flot-spline/jquery.flot.spline.js') }}"></script>
+     <script src="{{ asset('backend/lib/medium-editor/medium-editor.js') }}"></script>
+     <script src="{{ asset('backend/lib/summernote/summernote-bs4.min.js') }}"></script>
+          
+     <script src="{{ asset('backend/js/starlight.js') }}"></script>
+    <script src="{{ asset('backend/js/ResizeSensor.js') }}"></script>
+    
+   
   </body>
 </html>
