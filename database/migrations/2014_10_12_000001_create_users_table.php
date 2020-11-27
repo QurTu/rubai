06f2288aaedb176800e985e85cfe11ca8d47a4f5
QurTu <martis55@inbox.lt>
2020-11-27 19:37:58 +0200

    one more try

--- database/migrations/2014_10_12_000001_create_users_table.php
+++ database/migrations/2014_10_12_000001_create_users_table.php
@@ -16,8 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->unsignedBigInteger('role_id')->default(2);
+            $table->string('email')->unique(); 
+            $table->unsignedBigInteger('role_id')->default(2)->nullable();
             $table->foreign('role_id')->references('id')->on('roles');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
