c9ede273c85e88cb786507bf499fc4113798fbde
QurTu <martis55@inbox.lt>
2020-11-27 20:14:21 +0200

    last fix

--- resources/views/front-end/home.blade.php
+++ resources/views/front-end/home.blade.php
@@ -49,9 +49,7 @@
     </div>
 @endif
 
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-        cancel
-      </button>
+    
 	  @if($payment == 0)
     <!-- cancel payment model-->
     <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
